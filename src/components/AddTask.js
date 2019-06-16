5994015e6de5aceb0e54ee3ece5c0aa53efb9dac
Piotr Duklas <pedrosson@gmail.com>
2019-06-16 10:59:06 +0200

    add methods

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
 
 class AddTask extends Component {
+
+    minDate = new Date().toISOString().slice(0, 10);
+
     state = {
         text: "",
-        checked: false
+        checked: false,
+        date: this.minDate
     }
+
+    handleDate = (e) => {
+        this.setState({
+            date: e.target.value
+        })
+    }
+
+    handleText = (e) => {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    handleCheckbox = (e) => {
+        this.setState({
+            checked: e.target.checked
+        })
+    }
+
+    handleClick = () => {
+
+    }
+
     render() {
+
+        let maxDate = this.minDate.slice(0, 4) * 1 + 1;
+        maxDate = maxDate + "-12-31"
         return (
             <>
-                <input type="text" value={this.state.text} placeholder="wpisz coś" />
-                <label htmlFor=""> Priorytet </label>
-                <input type="checkbox" name="priorytet" id="" checked={this.state.checked} />
-                <label htmlFor="">Deadline: </label>
-                <input type="date" name="" id="" min="2019-06-14" max="2019-12-31" />
-                <button>Dodaj zadanie do listy</button>
+                <input type="text" placeholder="wpisz coś" value={this.state.text} onChange={this.handleText} />
+                <input type="checkbox" checked={this.state.checked} id="important" onChange={this.handleCheckbox} />
+                <label htmlFor="important"> Priorytet </label>
+                <label htmlFor="date">Deadline: </label>
+                <input type="date" value={this.state.date} min={this.minDate} max={maxDate} onChange={this.handleDate} />
+                <button onClick={this.handleClick}>Dodaj zadanie do listy</button>
                 <hr />
             </>
         );
